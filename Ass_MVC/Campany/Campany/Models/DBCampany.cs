b6f198696fcc94e2288651fb9569a24b0448988c
Dungtan2208013 <112045587+Dungtan2208013@users.noreply.github.com>
2023-10-25 11:41:24 +0700

    .

---
+++ Ass_MVC/Campany/Campany/Models/DBCampany.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+namespace Campany.Models
+{
+    public class DBCampany : DbContext
+    {
+        public DBCampany(DbContextOptions<DBCampany> options)
+           : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-7EGMHQLE\\SQLEXPRESS;Initial Catalog=Company_MVC;User ID=sa;Password=123456;Encrypt=False");
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<User>(entity => {
+                entity.HasKey(k => k.Id);
+            });
+        }
+    }
+}
---
+++ Ass_MVC/Campany/Campany/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Campany.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string? EmployeeName { get; set; }
+        public string? EmployeeCode { get; set; }
+        public int DepartmentId { get; set; }
+        public string? Rank { get; set; }
+    }
+}
